8bd1fbc3b52840741b2be0e30c71fdf0f3c11a64
Sushi6006 <lin.terry.x2@gmail.com>
2019-12-21 20:46:08 +0800

    added main.py

---
+++ main.py
@@ -0,0 +1,114 @@
+from tkinter import *
+
+
+def get_size():
+    pass
+
+def insert():
+    pass
+
+def remove_max():
+    pass
+
+def get_max():
+    pass
+
+def heap_sort():
+    pass
+
+def build_heap_by_file():
+    pass
+
+
+class GUI:
+    WIDTH = 600
+    HEIGHT = 400
+    TITLE = "THE FULL MARK HEAP SORT"
+    FONT = ("Courier", 40)
+
+    def __init__(self):
+
+        # init window
+        self.root = Tk()
+        self.root.title(self.TITLE)
+        self.root.geometry("{}x{}".format(self.WIDTH, self.HEIGHT))
+        self.root.resizable(False, False)
+        self.current_frame = None
+
+        # create windows
+        self.main_page = MainPage(self.root, self)
+        self.menu_page = MenuPage(self.root, self)
+        #TODO: self.insert_page =
+
+        # mainloop
+        self.root.mainloop()
+
+    #TODO: make original page optional?
+    #TODO: a way to unpack whatever is packed?
+    def switch_page(self, original_page, new_page):
+        original_page.main_frame.pack_forget()
+        new_page.main_frame.pack()
+
+
+class MainPage:
+    def __init__(self, window, gui_object):
+        # load info from parents
+        self.root = window
+        self.gui_object = gui_object
+
+        # create elements on the main page aka welcome page
+        self.main_frame = Frame(self.root)
+        self.title = Label(self.main_frame, text="THE FULL MARK HEAP SORT", font=GUI.FONT)
+        self.start_button = Button(self.main_frame, text="Mark: 120%...? (Start)", command=self.start)
+        self.title.pack()
+        self.start_button.pack()
+
+    def start(self):
+        self.gui_object.switch_page(self, self.gui_object.menu_page)
+
+
+class MenuPage:
+    def __init__(self, window, gui_object):
+        # load info from parents
+        self.root = window
+        self.gui_object = gui_object
+
+        # create elements for the menu
+        self.main_frame = Frame(self.root)
+        self.menu_title = Label(self.main_frame, text="")
+
+        # get size
+        self.size_frame = Frame(self.main_frame)
+        self.size_button = Button(self.size_frame, text="Get Size", command=self.get_size)
+
+        # insert
+        self.insert_frame = Frame(self.main_frame)
+        self.insert_label = Label(self.insert_frame, text="Number to Insert:")
+        self.insert_entry = Entry(self.insert_frame)
+        self.insert_button = Button(self.insert_frame, text="Insert", command=self.insert_node)
+        #TODO: grid insert frame
+
+        # remove max
+        self.remove_max_frame = Frame(self.main_frame)
+        self.remove_max_button = Button(self.remove_max_frame, text="Remove Max", command=self.remove_max)
+
+        # get Max
+        self.get_max_frame = Frame(self.main_frame)
+        self.get_max_button = Button(self.get_max_frame, text="Get Max", command=self.get_max)
+
+        # sort
+        self.sort_frame = Frame(self.main_frame)
+        self.sort_button = Button(self.sort_frame, text="Heap Sort", command=self.sort)
+
+        # build by file
+        self.build_frame = Frame(self.main_frame)
+        self.build_button = Button(self.build_button, text="Build Heap by File", command=self.build_heap)
+
+        # pack all the frames
+
+
+
+#TODO: status_page? make all above in a notification window?
+
+
+GUI()
